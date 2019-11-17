c02b18febb1e80eabb79ac3455a75706f58748ee
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-11-17 02:15:16 -0500

    se agrego el 9no ejercicio"

---
+++ Damian/condicionales_simples/condicional_simple_09.py
@@ -0,0 +1,31 @@
+import os
+#declarar las variables
+velocidad_1,velocidad_2,distancia=0.0,0.0,0.0
+tiempo_previsto=False
+
+#input
+velocidad_1=int(os.sys.argv[1])
+velocidad_2=int(os.sys.argv[2])
+distancia=int(os.sys.argv[3])
+
+#processing
+tiempo_encuentro=(distancia)/(velocidad_1+velocidad_2)
+tiempo_previsto=(tiempo_encuentro>10)
+
+#output
+print("####################################################")
+print("#    CALCULO DEL TIEMPO DE ENCUENTRO     #")
+print("####################################################")
+print("#")
+print("#velocidad primer movil:", velocidad_1, "m/s")
+print("#velocidad segundo movil:", velocidad_2, "m/s")
+print("#distancia de separacion:", distancia, "metros")
+print("#tiempo de encuentro:", tiempo_encuentro)
+print("#")
+print("#####################################################")
+
+#condicional
+if(tiempo_encuentro>10):
+    print("se encontraron en el tiempo previsto")
+
+#fin_if
