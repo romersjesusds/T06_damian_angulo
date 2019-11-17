4dd80f51bc1a73b779312242597261fbdee78a73
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-11-17 02:12:54 -0500

    se agrego el 8vo ejercicio"

---
+++ Damian/condicionales_simples/condicional_simple_08.py
@@ -0,0 +1,34 @@
+import os
+#declarar las variables
+usuario,operadora_internet,gigas_usadas,costo_giga="","",0.0,0.0
+gigas_limite=False
+
+#input
+usuario=os.sys.argv[1]
+operadora_internet=os.sys.argv[2]
+gigas_usadas=int(os.sys.argv[3])
+costo_giga=float(os.sys.argv[4])
+total=gigas_usadas*costo_giga
+igv=total*0.18
+total_pagar=total+igv
+gigas_limite=(gigas_usadas>50)
+
+#output
+print("#############################################################")
+print("#	BITEL- TELEFONIA DE TODOS			                   #")
+print("#############################################################")
+print("cliente:", usuario,             "operadora:", operadora_internet)
+print("#############################################################")
+print("#")
+print("#gigas usadas:", gigas_usadas)
+print("#costo por giga:", costo_giga)
+print("#total:", total)
+print("#IGV:", igv)
+print("total a pagar:", total_pagar)
+print("#############################################################")
+
+#condicional
+if(gigas_usadas>50):
+    print("por favor no vuelva a superar el limite")
+
+#fin_if
